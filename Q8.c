dee27a13e3858207565a8da9946bdd2728c429e5
Yogya-Chugh <yogyachugh10@gmail.com>
2023-10-21 04:05:36 +0530

    Q8

---
+++ Q8.c
@@ -0,0 +1,14 @@
+//PROGRAM TO SWAP TWO NUMBERS USING ANOTHER VARIABLE
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter a: ");
+    scanf("%d",&a);
+    printf("Enter b: ");
+    scanf("%d",&b);
+    c=a;
+    a=b;
+    b=c;
+    printf("a = %d , b = %d",a,b);
+    return 0;
+}
